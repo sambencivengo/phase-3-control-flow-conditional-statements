122050322642c7693dacb9e1ff6d3be23ce935bb
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-01 10:52:21 -0400

    commit tree 3a9842cf73eb88940d568ece2ff58e1d1c929bc4

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,61 @@
 def admin_login(username, password)
-  # your code here
-end
+  if username.downcase === "admin" && password === "12345"
+     "Access granted"
+    else
+      "Access denied"
+    end
+  end
+
 
 def hows_the_weather(temperature)
-  # your code here
+  response = ""
+  if temperature < 40
+    response = "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    response =  "It's a little chilly out there!"
+  elsif temperature > 85
+    response = "It's too dang hot out there!"
+  else 
+    response =  "It's perfect out there!"
+  end
+  response
 end
 
+# Write a method #fizzbuzz takes in a number. For multiples of three, return "Fizz" instead of the number. For the multiples of five, return "Buzz". For numbers which are multiples of both three and five, return "FizzBuzz". For all other numbers, just return the number itself.
 def fizzbuzz(num)
-  # your code here
+  result = ""
+  if num % 3 === 0 && num % 5 === 0
+    result = "FizzBuzz"
+  elsif num % 3 === 0
+    result = "Fizz"
+  elsif num % 5 === 0
+    result = "Buzz"
+  else
+    result = num
+  end
+  result
 end
+
+
 
 def calculator(operation, num1, num2)
-  # your code here
+  result = ""
+ case operation
+ when "+"
+  result = num1 + num2
+ when "-"
+  result = num1 - num2
+ when "*"
+  result = num1 * num2
+ when "/"
+  result = num1 / num2
+ else 
+  puts "Invalid operation!"
+  result = nil
+ end
+ result
 end
+
+
+
 
